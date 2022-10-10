b885df2736318131d6cc25196f489584c4b1e00e
IrishTnT <30432894+IrishTnT@users.noreply.github.com>
2022-10-10 22:41:06 +0100

    Added Stations

--- IrishRail.py
+++ IrishRail.py
@@ -6,24 +6,50 @@ import xml.etree.ElementTree as ET
 import urllib
 import numpy
 
-# Gets which train type the User wishes to read.
-type = input("What type of train do you wish to search for?\nM - Mainline,\nD - DART,\nS- Suburban,\nA - All.\n(Any other input will search all trains.)\n")
+## To-Do:
+# -> Show all trains.
+# -> Fix input checks.
+# -> Stop errors.
+# -> Do basic error correction. (Take Station list & compare?)
 
-if type.upper() == "M":
-    print("Selected MAINLINE trains. Please wait...")
-    requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=M'
-elif type.upper() == "D":
-    print("Selected DART trains. Please wait...")
-    requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=D'
-elif type.upper() == "S":
-    print("Selected SUBURAN trains. Please wait...")
-    requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=S'
-elif type.upper() == "A":
-    print("Selected ALL trains. Please wait...")
-    requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=A'
+
+mreq = input("Which do you want to query?\nS - Stations,\nT - Trains.\n(Any other input will search for Trains.)\n")
+
+if mreq.upper() == "S":
+    print("\n\n---==<[STATIONS]>==---")
+    tloc = 0
+elif mreq.upper() == "T":
+    print("\n\n---==<[TRAINS]>==---")
+    tloc = 1
 else:
-    print("Defaulted to ALL trains. Please wait...")
-    requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=A'
+    print("\n\n---==<[TRAINS]>==---")
+    tloc = 1
+
+if tloc == 1:
+    # Gets which train type the User wishes to read.
+    type = input("What type of train do you wish to search for?\nM - Mainline,\nD - DART,\nS- Suburban,\nA - All.\n(Any other input will search all trains.)\n")
+
+    if type.upper() == "M":
+        print("Selected MAINLINE trains. Please wait...")
+        requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=M'
+    elif type.upper() == "D":
+        print("Selected DART trains. Please wait...")
+        requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=D'
+    elif type.upper() == "S":
+        print("Selected SUBURAN trains. Please wait...")
+        requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=S'
+    elif type.upper() == "A":
+        print("Selected ALL trains. Please wait...")
+        requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=A'
+    else:
+        print("Defaulted to ALL trains. Please wait...")
+        requestURL = 'http://api.irishrail.ie/realtime/realtime.asmx/getCurrentTrainsXML_WithTrainType?TrainType=A'
+elif tloc == 0:
+    stype = input("What station do you want to query\n")
+    stime = input("How far into the future do you want to query?\n")
+
+    requestURL = 'https://api.irishrail.ie/realtime/realtime.asmx/getStationDataByNameXML_withNumMins?StationDesc=' + stype + '&NumMins=' + stime
+    print("Querying", stype.upper(), ",", stime, "minutes into the future. Please wait...")
 
 root = ET.parse(urllib.request.urlopen(requestURL)).getroot()
 
@@ -54,30 +80,42 @@ with open("./test.json", "w") as f:
     f.write(json.dumps(trainData, indent = 4, sort_keys = True))
     # This just dumps the data into a json file
 
-# Find the train you want
-dest = input("Loaded timetables.\nSelect your destination, or type 'cancel' to exit.\n")
-
-def extract_train_data(destination):
+if tloc == 0:
     train = []
     
-    ##for iterator in trainData:
-        
-    
-    train = next((item for item in trainData if item["Direction"].lower() == destination.lower()), None)
-    
-    if train:
-        return train["PublicMessage"].replace('\\n', '\n').splitlines()
-    return "N/A"
+    train = next((item for item in trainData))
 
-while extract_train_data(dest) == "N/A":
-    if dest == "cancel":
-        exit()
-        
-    if dest[0].lower() + dest[1].lower() == "to":
-        print("Train", dest, "not found!")
+    if int(train["Duein"]) != 1:
+        print("Train from", train["Origin"], "to", train["Direction"], "arriving in", train["Duein"], "minutes. Running", train["Late"], "minutes late.")
     else:
-        print("Train to", dest, "not found!")
-    dest = input("Select your destination, or type 'cancel' to exit.\n ")
+        print("Train from", train["Origin"], "to", train["Direction"], "arriving in", train["Duein"], "minute. Running", train["Late"], "minutes late.")
 
-if extract_train_data(dest) != "N/A":
-    print("The next train is the", extract_train_data(dest)[1], "[" + extract_train_data(dest)[0] + "].", extract_train_data(dest)[2] + ".")
+elif tloc == 1:
+    # Find the train you want
+    dest = input("Loaded timetables.\nSelect your destination, or type 'cancel' to exit.\n")
+
+    def extract_train_data(destination):
+        train = []
+        
+        ##for iterator in trainData:
+
+            
+        
+        train = next((item for item in trainData if item["Direction"].lower() == destination.lower()), None)
+        
+        if train:
+            return train["PublicMessage"].replace('\\n', '\n').splitlines()
+        return "N/A"
+
+    while extract_train_data(dest) == "N/A":
+        if dest == "cancel":
+            exit()
+            
+        if dest[0].lower() + dest[1].lower() == "to":
+            print("Train", dest, "not found!")
+        else:
+            print("Train to", dest, "not found!")
+        dest = input("Select your destination, or type 'cancel' to exit.\n ")
+
+    if extract_train_data(dest) != "N/A":
+        print("The next train is the", extract_train_data(dest)[1], "[" + extract_train_data(dest)[0] + "].", extract_train_data(dest)[2] + ".")
