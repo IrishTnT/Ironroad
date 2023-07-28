95079b10c48fe59e3f2080edf0eb6962fbe4c9d1
Ruán <ruan@murgatroyd.me>
2023-07-28 01:52:35 +0100

    Move to Svelte, initial work

--- readme.md
+++ readme.md
@@ -1,8 +1,38 @@
-<img height=110 width=500 src="ironroad.png">
+# create-svelte
 
-### Ironroad, now on web!
+Everything you need to build a Svelte project, powered by [`create-svelte`](https://github.com/sveltejs/kit/tree/master/packages/create-svelte).
 
-Ironroad is a personal project to further my Python experience by processing the [IrishRail API](http://api.irishrail.ie/realtime/).
-Ironroad is available under the GNU General Public License V3
+## Creating a project
 
-Ironroad, originally a CLI application, is now available on the web! The web version is available [here](https://rail.irishtnt.com/).
+If you're seeing this, you've probably already done this step. Congrats!
+
+```bash
+# create a new project in the current directory
+npm create svelte@latest
+
+# create a new project in my-app
+npm create svelte@latest my-app
+```
+
+## Developing
+
+Once you've created a project and installed dependencies with `npm install` (or `pnpm install` or `yarn`), start a development server:
+
+```bash
+npm run dev
+
+# or start the server and open the app in a new browser tab
+npm run dev -- --open
+```
+
+## Building
+
+To create a production version of your app:
+
+```bash
+npm run build
+```
+
+You can preview the production build with `npm run preview`.
+
+> To deploy your app, you may need to install an [adapter](https://kit.svelte.dev/docs/adapters) for your target environment.
---
+++ src/routes/test/+page.svelte
@@ -0,0 +1,65 @@
+<script lang="ts">
+    import type { PageData } from './$types';
+    
+    export let data: PageData;
+</script>
+
+    <div class="container">
+        <img src="./ironroad.png">
+        <h1 class="title large nomargin">Test for all CSS elements</h1>
+        <h1 class="medium nomargin">MEDIUM TEXT</h1>
+        <h1 class="small nomargin">SMALL TEXT</h1>
+
+        <p>Plain text</p>
+
+        <div class="exterior">
+            <h1 class="title large">The Form</h1>
+            <div class="interior">
+                <form>
+                    <!--Dropdown-->
+                    <div class="form-element dropdown">
+                        <select>
+                            <option value="1">Option 1</option>
+                            <option value="2">Option 2</option>
+                        </select>
+                    </div>
+
+                    <!--Input-->
+                    <div class="form-element input">
+                        <input type="text" placeholder="Input">
+                    </div>
+
+                    <!--Button-->
+                    <div class="form-element button">
+                        <button>Button</button>
+                    </div>
+
+                    <!--Checkbox-->
+                    <div class="form-element checkbox">
+                        <input type="checkbox" id="checkbox">
+                        <label for="checkbox">Checkbox</label>
+                    </div>
+
+                    <!--Switch-->
+                    <div class="form-element switchbox">
+                        <label class="switch">
+                            <input type="checkbox" class="switchbox">
+                            <span class="switchslider"></span>
+                        </label>
+                    </div>
+
+                    <!--Slider-->
+                    <div class="form-element slider">
+                        <input type="range" id="slider">
+                        <label for="slider">Slider</label>
+                    </div>
+
+                    <!--Textarea-->
+                    <div class="form-element textarea">
+                        <textarea placeholder="Textarea"></textarea>
+                    </div>
+                    
+                </form>
+            </div>
+        </div>
+    </div>
